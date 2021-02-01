aee3b90a7616eb9c16cb726bbd0823d9bf6c24e8
Matias Griese <matias@trilbymedia.com>
2021-02-01 10:39:33 +0200

    Release 5.4.37 - Automatic Version Number Updates

--- src/RealLoader.php
+++ src/RealLoader.php
@@ -45,8 +45,8 @@ abstract class RealLoader
             throw new \LogicException(self::$errorMessageGantryLoaded);
         }
 
-        define('GANTRY5_VERSION', '@version@');
-        define('GANTRY5_VERSION_DATE', '@versiondate@');
+        define('GANTRY5_VERSION', '5.4.37');
+        define('GANTRY5_VERSION_DATE', 'February  1, 2021');
 
         if (!defined('DS')) {
             define('DS', DIRECTORY_SEPARATOR);
